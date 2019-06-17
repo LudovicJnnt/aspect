2d927dbcd6455e83f28b4e891b86404db53465cf
Ludovic Jeanniot <jeanniot@login01.cluster>
2019-06-17 15:31:41 +0200

    test MOHO benchmark

--- source/initial_composition/crust1.cc
+++ source/initial_composition/crust1.cc
@@ -58,8 +58,8 @@ namespace aspect
         {
           
           // Get coordinates (surface elevation, longitude (0-360), latitude (-90 - 90))
-          //std::array<double,dim> wcoord = Utilities::Coordinates::WGS84_coordinates(position);
-          const std::array<double,dim> scoord = Utilities::Coordinates::cartesian_to_spherical_coordinates(position);
+          std::array<double,dim> scoord = Utilities::Coordinates::WGS84_coordinates(position);
+          //const std::array<double,dim> scoord = Utilities::Coordinates::cartesian_to_spherical_coordinates(position);
                 
           // Define location of current point
           //const double depth  = wcoord[0] - position.norm(); // positive value 
@@ -78,35 +78,35 @@ namespace aspect
           double cr1_rho = 0.;
           if (depth < bnds[cr1_index][8])
             {
-              cr1_rho = 3.3;
+              cr1_rho = .450; //3.3;
             }
           else if (depth < bnds[cr1_index][7])
             {
-              cr1_rho = rho[cr1_index][7]; 
+              cr1_rho = 0; //rho[cr1_index][7]; 
             }
           else if (depth < bnds[cr1_index][6])
             {
-              cr1_rho = rho[cr1_index][6];
+              cr1_rho = 0; //rho[cr1_index][6];
             } 
           else if (depth < bnds[cr1_index][5])
             {
-              cr1_rho = rho[cr1_index][5];
+              cr1_rho = 0; //rho[cr1_index][5];
             }
           else if (depth < bnds[cr1_index][4])
             {
-              cr1_rho = rho[cr1_index][4];
+              cr1_rho = 0; //rho[cr1_index][4];
             }
           else if (depth < bnds[cr1_index][3])
             {
-              cr1_rho = rho[cr1_index][3];
+              cr1_rho = 0; //rho[cr1_index][3];
             }
           else if (depth < bnds[cr1_index][2])
             {
-              cr1_rho = rho[cr1_index][2];
+              cr1_rho = 0; //rho[cr1_index][2];
             }
           else if (depth < bnds[cr1_index][1])
             {
-              cr1_rho = rho[cr1_index][1];
+              cr1_rho = 0; //rho[cr1_index][1];
             }
           else 
             {
